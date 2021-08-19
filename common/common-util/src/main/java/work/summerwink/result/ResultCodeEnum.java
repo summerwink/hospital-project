e2ed7dc062c0f5736b0dfc34ea7cc2b2a9852876
LeePoseidon <904628045@qq.com>
2021-08-19 13:36:34 +0800

    init projcect

---
+++ common/common-util/src/main/java/work/summerwink/result/ResultCodeEnum.java
@@ -0,0 +1,22 @@
+package work.summerwink.result;
+
+import lombok.Getter;
+
+/**
+ * 统一返回结果状态信息类
+ */
+@Getter
+public enum ResultCodeEnum {
+
+    SUCCESS(200,"成功"),
+    ERROR(201, "失败"),
+    ;
+
+    private Integer code;
+    private String message;
+
+    private ResultCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
